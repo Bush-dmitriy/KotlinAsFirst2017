3fd54607702cb8c8d079ed0ae60e5d6c91cf0f82
Bush-dmitriy <bush-dmitriy@yandex.ru>
2017-10-02 00:36:55 +0300

    lesson 3 в процессе

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -18,7 +18,9 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = (number / 1000 + number / 100 % 10 == number  / 10 % 10 + number % 10)
+fun isNumberHappy(number: Int): Boolean =
+        number / 1000 + number / 100 % 10 ==
+                number / 10 % 10 + number % 10
 
 /**
  * Простая
@@ -28,7 +30,7 @@ fun isNumberHappy(number: Int): Boolean = (number / 1000 + number / 100 % 10 == 
  * Считать, что ферзи не могут загораживать друг друга.
  */
 fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
-        (x1 == x2 || y1 == y2 || Math.abs(x1 - x2) == Math.abs(y1 - y2))
+        x1 == x2 || y1 == y2 || Math.abs(x1 - x2) == Math.abs(y1 - y2)
 
 /**
  * Средняя
@@ -39,7 +41,7 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
                  x2: Double, y2: Double, r2: Double): Boolean =
-        (r2 >= r1 + Math.sqrt(sqr(Math.abs(y1 - y2)) + sqr(Math.abs(x1 - x2))))
+        r2 >= r1 + Math.sqrt(sqr(Math.abs(y1 - y2)) + sqr(Math.abs(x1 - x2)))
 
 /**
  * Средняя
--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -1,4 +1,5 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson3.task1
 
 /**
@@ -34,7 +35,7 @@ fun isPrime(n: Int): Boolean {
  */
 fun isPerfect(n: Int): Boolean {
     var sum = 1
-    for (m in 2..n/2) {
+    for (m in 2..n / 2) {
         if (n % m > 0) continue
         sum += m
         if (sum > n) break
@@ -60,7 +61,17 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  * Найти количество цифр в заданном числе n.
  * Например, число 1 содержит 1 цифру, 456 -- 3 цифры, 65536 -- 5 цифр.
  */
-fun digitNumber(n: Int): Int = TODO()
+fun digitNumber(n: Int): Int {
+    if (n == 0) return 1 else {
+        var result = 0
+        var k = n
+        while (k > 0) {
+            k /= 10
+            result++
+        }
+        return result
+    }
+}
 
 /**
  * Простая
@@ -68,7 +79,8 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+fun fib(n: Int): Int =
+        if (n <= 2) 1 else fib(n - 2) + fib(n - 1)
 
 /**
  * Простая
@@ -76,21 +88,42 @@ fun fib(n: Int): Int = TODO()
  * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
  * минимальное число k, которое делится и на m и на n без остатка
  */
-fun lcm(m: Int, n: Int): Int = TODO()
+fun lcm(m: Int, n: Int): Int {
+    var k = 0
+    for (i in 1..m * n) {
+        k++
+        if (k % m == 0 && k % n == 0) break
+    }
+    return k
+}
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
-fun minDivisor(n: Int): Int = TODO()
+fun minDivisor(n: Int): Int {
+    var l=2
+    for (i in 2..n) {
+        if (n % i == 0) break
+        l++
+    }
+    return l
+}
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    var l=n-1
+    for (i in n-1 downTo 1) {
+        if (n % i == 0) break
+        l--
+    }
+    return l
+}
 
 /**
  * Простая
@@ -99,7 +132,14 @@ fun maxDivisor(n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean  {
+    var k = 1
+    for (i in 2..m) {
+        k++
+        if (m % k == 0 && n % k == 0) return false
+    }
+    return true
+}
 
 /**
  * Простая
